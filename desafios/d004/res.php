72f25ba8e4554cdfd78ff7538aefa02870370609
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-09-24 20:56:39 -0300

    Update res.php

--- desafios/d004/res.php
+++ desafios/d004/res.php
@@ -3,18 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conversor Básico</title>
+    <title>Conversor Avançado</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <section>
-        <h1>Conversor Básico v1.0</h1>
+        <h1>Conversor Avançado v2.0</h1>
         <?php 
-            $real = $_GET["num"] ?? 0;
-            $cotacao = 4.93;
-            $din = $real / $cotacao;
-            $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
-            echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a  " . numfmt_format_currency($padrao, $din, "USD" . "</p>");
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'09-17-2023\'&@dataFinalCotacao=\'09-24-2023\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+            
+            var_dump($dados);
+
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+            echo "A cotação é $cotacao";
         ?>
         <button type="button" onclick="javascript:history.go(-1)">Voltar</button>
     </section>
