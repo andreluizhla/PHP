6864c9cdea33d46293c08a92803d80a9a5da731b
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-10-28 21:30:12 -0300

    desafio 5

--- desafios/d005/res.php
+++ desafios/d005/res.php
@@ -7,16 +7,20 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <header>
-        <h1>Analizando um Número Real</h1>
-    </header>
     <section>
+        <h1>Analizando um Número Real</h1>
         <?php 
             $num = $_REQUEST["num"] ?? 0;
-            echo "<p>Analizando o número $num informado pelo usuário:</p>";
+            echo "<p>Analizando o número <strong>". number_format($num, 3, ",", ".") ."</strong> informado pelo usuário:</p>";
             
-            $abs = abs($num);
+            $int = (int) $num;
+            $fra = $num - $int;
+            echo "<ul>
+                <li>A parte inteira do número é <strong>". number_format($int, 0, ",", ".") ."</strong></li>
+                <li>A parte fracionária do número é <strong>".number_format($fra, 3, ",", ".")."</strong></li>
+            </ul>"
         ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
     </section>
 </body>
 </html>
